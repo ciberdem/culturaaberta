b2993cbfc4ec0e41aa37e827334c8795f9a112fd
patrickbarroso <37444862+patrickbarroso@users.noreply.github.com>
2018-05-11 12:47:14 -0300

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,11 @@
+<table id="tbfooter" >
+	<tr>
+		<td>© CULTURA ABERTA - As OSCIPs de Cultura do Rio de Janeiro. CIBERDEM. UNIRIO. 2018</td>
+	</tr>
+</table>  
+<script defer="defer" src="//barra.brasil.gov.br/barra.js" type="text/javascript"></script>
+<map name="menumap" id="bordermap" >
+  <area alt="Ciberdem" title="Ciberdem" href="http://ciberdem.uniriotec.br/" target="_blank" shape="rect" coords="6,2,120,200" / onClick="this.blur();">
+  <area alt="Home" title="Home" href="./" target="_self" shape="rect" coords="120,1,860,226" />
+  <area alt="Unirio" title="Unirio" href="http://www.unirio.br/" target="_blank" shape="rect" coords="870,4,1947,228" onClick="this.blur();" />
+</map>
---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!-- BARRA DO GOVERNO FEDERAL -->
+<div id="barra-brasil">
+
+    <a href="http://brasil.gov.br" style="height: 20px; padding:4px 0 4px 10px; display: block; font-family:sans,sans-serif; text-decoration:none; color:white; ">Portal do Governo Brasileiro</a>
+
+</div>
+<div align="center" ><img src="img/banner_cultura1.jpg" width="1010" usemap="#menumap"></div>
+<!-- MENU DE NAVEGAÇÃO -->
+	<table id="tbmenu" >
+<tr><td><a href="./" id="tbmenulink">HOME</a></td>
+    <td><a href="./sobre.php" id="tbmenulink">&nbsp;&nbsp;SOBRE</a></td>
+	<td><a href="./oscips.php" id="tbmenulink">&nbsp;AS OSCIPS</a></td>
+	<td><a href="./projetos.php" id="tbmenulink">PROJETOS E CONVÊNIOS</a></td>
+	<td><a href="./noticias.php" id="tbmenulink">NOTÍCIAS</a></td>
+	<td><a href="./particip.php" id="tbmenulink">EDITAIS E PARTICIPAÇÃO</a></td> 
+	<td><a href="./dados.php" id="tbmenulink">DADOS ABERTOS</a>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+</tr>
+</table> 
